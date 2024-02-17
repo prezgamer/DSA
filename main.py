3f6fa6ec0d9558dfac1d914fdc168c48df7cc7b9
prezgamer <50046059+prezgamer@users.noreply.github.com>
2024-02-17 16:56:28 +0800

    V1.0.0
    Added Routes.json and main.py

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
+# import json
+# import os
+# print(os.getcwd())
+
+# # Define the file name
+# file_name = open("C:/Users/ffjan/OneDrive/Desktop/DSA Project/DSA/Routes.json")
+
+# # returns JSON object as 
+# # a dictionary
+# data = json.load(file_name)
+ 
+# # Iterating through the json
+# # list
+# for i in data["AirlineDetails"]:
+#     print(i)
+
 import json
+import os
+print(os.getcwd())
 
-# Define the file name
-file_name = "C:\Users\ffjan\OneDrive\Desktop\Python\DSA\Routes.json"
+file_name = 'C:/Users/ffjan/OneDrive/Desktop/DSA Project/Routes.json'
+print(file_name)
 
-# Open the file in read mode
-with open(file_name, "r") as file:
-    # Read the contents of the file
-    routes_data = json.load(file)
+try:
+    # Open the file in read mode
+    with open(file_name, "r") as file:
+        # Read the contents of the file
+        routes_data = json.load(file)
+        print(routes_data)
+except Exception as e:
+    print("Error loading JSON file:", e)
 
-# Now you can work with the data loaded from the JSON file
-# For example, you can print it
-print(routes_data)
+# try:
+#     # Open the file in read mode
+#     with open('Routes.json', 'r', encoding='utf-8') as file:
+#         # Read the contents of the file
+#         routes_data = json.load(file)
+#         print(routes_data)
+
+# except FileNotFoundError:
+#     print(f"File '{'C:/Users/ffjan/OneDrive/Desktop/DSA Project/DSA/Routes.json'}' not found.")
+# except json.JSONDecodeError as e:
+#     print(f"Error decoding JSON file '{'C:/Users/ffjan/OneDrive/Desktop/DSA Project/DSA/Routes.json'}': {e}")
+# except Exception as e:
+#     print(f"Error loading JSON file '{'C:/Users/ffjan/OneDrive/Desktop/DSA Project/DSA/Routes.json'}': {e}")
